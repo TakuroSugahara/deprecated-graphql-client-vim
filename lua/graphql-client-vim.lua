15371722fa622dafe09cc6e40f55dc2e0670beee
TakuroSugahara <k5690033@gmail.com>
2023-03-12 17:24:53 +0900

    コマンド実行時にバッファを上書きする処理

--- lua/graphql-client-vim.lua
+++ lua/graphql-client-vim.lua
@@ -16,16 +16,28 @@ function M.open_output()
 
   -- local buf = api.nvim_create_buf(false, true)
   api.nvim_buf_set_option(0, 'buftype', 'nofile') -- 書き込み禁止
-  api.nvim_buf_set_option(0, 'modifiable', false) -- 書き込み禁止
+  -- api.nvim_buf_set_option(0, 'modifiable', false) -- 書き込み禁止
   api.nvim_buf_set_option(0, 'swapfile', false) -- swrapfile作成しない
   api.nvim_buf_set_option(0, 'bufhidden', 'wipe') -- bufferを非表示にしたら削除する
+  api.nvim_command("hidden")
 end
 
--- local count = 0
--- function M.execute_request()
---   api.nvim_out_write("Execute graphql request")
---   api.nvim_buf_set_lines(ouput_buffer_name, 0, 1, false, {"hoge: " .. count, "piyo"})
--- end
+local count = 0
+function M.execute_request()
+  api.nvim_out_write("Execute graphql request")
+
+  local new_lines = {}
+  table.insert(new_lines, "hoge")
+
+  count = count + 1
+  for i = 1, count do
+    table.insert(new_lines, "piyo")
+  end
+
+  table.insert(new_lines, "count: " .. count)
+
+  api.nvim_buf_set_lines(0, 0, -1, false, new_lines)
+end
 
 -- -- plugin内のmoduleの関数を実行するパターン
 -- function M.hello_submodule()
