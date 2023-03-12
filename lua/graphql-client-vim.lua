c9c70fd3bf3ceabb2945d6b4850959c8db8bd935
TakuroSugahara <k5690033@gmail.com>
2023-03-12 16:51:21 +0900

    ouput.jsonをbufferで表示する

--- lua/graphql-client-vim.lua
+++ lua/graphql-client-vim.lua
@@ -1,10 +1,31 @@
 local M = {}
 
+local api = vim.api
+local ouput_buffer_name = "ouput.json"
 -- local m = require('hello-beautiful-world-vim/module')
 
 function M.edit_http_header()
-  vim.api.nvim_out_write('Edit http header!\n')
+  api.nvim_out_write('Edit config file.\n')
+  api.nvim_command('setlocal buftype=nofile header.json')
 end
+
+function M.open_output()
+  api.nvim_out_write("Open " .. ouput_buffer_name .. ".\n")
+
+  api.nvim_command("botright vnew " .. ouput_buffer_name)
+
+  -- local buf = api.nvim_create_buf(false, true)
+  api.nvim_buf_set_option(0, 'buftype', 'nofile') -- 書き込み禁止
+  api.nvim_buf_set_option(0, 'modifiable', false) -- 書き込み禁止
+  api.nvim_buf_set_option(0, 'swapfile', false) -- swrapfile作成しない
+  api.nvim_buf_set_option(0, 'bufhidden', 'wipe') -- bufferを非表示にしたら削除する
+end
+
+-- local count = 0
+-- function M.execute_request()
+--   api.nvim_out_write("Execute graphql request")
+--   api.nvim_buf_set_lines(ouput_buffer_name, 0, 1, false, {"hoge: " .. count, "piyo"})
+-- end
 
 -- -- plugin内のmoduleの関数を実行するパターン
 -- function M.hello_submodule()
--- plugin/graphql-client-vim.vim
+++ plugin/graphql-client-vim.vim
@@ -3,5 +3,26 @@ if exists("g:loaded_graphql_client_vim")
 endif
 let g:loaded_graphql_client_vim = 1
 
-" Edit http header.json buffer
-command! -nargs=0 EditHttpHeader lua require("graphql-client-vim").edit_http_header()
+" Edit http header.json file
+command! -nargs=0 GraphqlClientEditConfig lua require("graphql-client-vim").edit_http_header()
+
+" Open output.json buffer
+command! -nargs=0 GraphqlClientOpenOutput lua require("graphql-client-vim").open_output()
+
+" Execute graphql request
+command! -nargs=0 GraphqlClientExecute lua require("graphql-client-vim").execute_request()
+
+" EchoをOutput.jsonを吐き出す
+" Graphqlのリクエストを送る
+" レスポンスをoutput.jsonに吐き出す
+" 実行コマンドでリクエストを呼び出す
+" .graphqlのファイルを編集
+" 編集した.graphqlのデータをリクエストとして吐き出す
+" Header, Configのファイルを編集
+" Header, Endpoint情報をリクエストに付ける
+" GraphqlClientEditで必要なeditorを全て表示する
+" 現在のwindowをGraphql Editorに乗り換える
+
+" variableの情報をbuffer書き込めるようにする
+" variableの情報をリクエストに付与する
+
